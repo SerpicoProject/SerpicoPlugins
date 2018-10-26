8e5bfb2473db381a07b7135088cbb4c3a6358965
Maxime Nadeau <maxime.nadeau@outlook.com>
2018-10-26 10:02:21 -0400

    Removed the encode_html: true annotations

--- TestPlugin/routes.rb
+++ TestPlugin/routes.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 
 # List current reports
 get '/TestPlugin/hello' do
-	haml :'../plugins/TestPlugin/views/test_plugin', :encode_html => true
+	haml :'../plugins/TestPlugin/views/test_plugin'
 end
